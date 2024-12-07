ad76da1e62043bb831b26165e0cb8f86ee2f80d8
陈帅帅 <chenshuaishuai@chenshuaishuaideMac-mini.local>
2024-12-07 19:20:49 +0800

    mysql redis link

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
-const router = require('koa-router')()
+const router = require('koa-router')(),
+crawlerController = require("../controllers/Crawler");
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
     title: 'Hello Koa 2!'
   })
 })
+
+router.get("/crawler", crawlerController.crawlSlierData);
 
 module.exports = router
